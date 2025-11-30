a5d9495c4a056df0543d29eb71a7739dd41ea8c1
ry05 <yadramshankar@gmail.com>
2025-11-30 19:15:44 +0530

    add game loop

---
+++ asteroids-game/main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.vernum}")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        return
+        
+    while True:
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
